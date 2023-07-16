9903d0a1571a9470590d9857ecf14e3e462f9724
Waveless <itzwaveless@gmail.com>
2023-07-16 20:15:14 +0530

    Show loading Skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card overflow="hidden" borderRadius="10px">
+    <Card width="380px" overflow="hidden" borderRadius="10px">
       <Image src={getCroppedImageURL(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,24 +24,30 @@ interface GamesResponse {
 }
 
 const useGames = () => {
+  const [isLoading, setLoading] = useState(false);
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<GamesResponse>("/games", { signal: controller.signal })
-      .then(({ data }) => setGames(data.results))
+      .then(({ data }) => {
+        setGames(data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
